1e5735b0a97f22b77002f3a3710b3e6de525cd89
Hank Choi <zlzlqlzl1@naver.com>
2021-07-30 00:16:48 +0900

    Add solver.py

---
+++ waffle_recruit_server/solver/solver.py
@@ -0,0 +1,52 @@
+import subprocess
+
+# from celery import Celery
+
+# app = Celery('tasks', broker='pyamqp://guest:guest@localhost//')
+
+
+# @app.task
+def run(language, filename, prob_num):
+    if prob_num == 0:
+        test_case = open('../solve/problem0/testcase.txt', "r")
+        solution = open('../solve/problem0/solution.txt', "r")
+    elif prob_num == 1:
+        test_case = open('../solve/problem1/testcase.txt', "r")
+        solution = open('../solve/problem1/solution.txt', "r")
+    elif prob_num == 2:
+        test_case = open('../solve/problem2/testcase.txt', "r")
+        solution = open('../solve/problem2/solution.txt', "r")
+    elif prob_num == 3:
+        test_case = open('../solve/problem3/testcase.txt', "r")
+        solution = open('../solve/problem3/solution.txt', "r")
+    else:
+        raise Exception("problem number error")
+
+    if language == "python":
+        proc = subprocess.Popen(["python3", filename], stdout=subprocess.PIPE, stdin=test_case)
+    elif language == "java":
+        proc = subprocess.Popen(["java", filename], stdout=subprocess.PIPE, stdin=test_case)
+    elif language == "kotlin":
+        # FIXME
+        proc = subprocess.Popen(["cat", filename], stdout=subprocess.PIPE, stdin=test_case)
+    elif language == "nodejs":
+        proc = subprocess.Popen(["node", filename], stdout=subprocess.PIPE, stdin=test_case)
+    else:
+        raise Exception("language error")
+    try:
+        outs, errs = proc.communicate(timeout=2)
+    except subprocess.TimeoutExpired:
+        proc.kill()
+        raise Exception("timeout")
+
+    if outs.decode() == solution.read():
+        return "!!"
+    test_case.close()
+    solution.close()
+    raise Exception("Wrong answer")
+
+
+if __name__ == '__main__':
+    run("python", "../example/pr1.py", 1)
+    run("java", "../example/Main.java", 0)
+    run("nodejs", "../example/example.js", 0)
