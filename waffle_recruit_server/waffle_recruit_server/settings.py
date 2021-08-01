4d6dfa9f63db40560e40c61a180c845a48c92fc7
Hank Choi <zlzlqlzl1@naver.com>
2021-08-01 23:05:08 +0900

    Resolve conflicts

--- waffle_recruit_server/waffle_recruit_server/settings.py
+++ waffle_recruit_server/waffle_recruit_server/settings.py
@@ -121,3 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://recruit.wafflestudio.com",
+    "http://recruit.wafflestudio.com",
+    "http://localhost:3000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
