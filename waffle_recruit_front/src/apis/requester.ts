532d8c5f3e428b4a280eeb38af6746c60b503105
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-08-01 14:39:07 +0900

    Add xsrf settings

--- waffle_recruit_front/src/apis/requester.ts
+++ waffle_recruit_front/src/apis/requester.ts
@@ -3,3 +3,7 @@ import axios from 'axios';
 export const requester = axios.create({
   baseURL: 'http://15.165.48.17',
 });
+
+requester.defaults.xsrfCookieName = 'csrftoken';
+requester.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+requester.defaults.withCredentials = true;
