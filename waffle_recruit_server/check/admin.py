59482cdaaafad79b7df424a3542e87446c181935
Hank Choi <zlzlqlzl1@naver.com>
2021-08-03 02:21:51 +0900

    Update admin page

--- waffle_recruit_server/check/admin.py
+++ waffle_recruit_server/check/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from check.models import Solver
+from check.models import Solver, Profile
 
 admin.site.register(Solver)
+admin.site.register(Profile)
