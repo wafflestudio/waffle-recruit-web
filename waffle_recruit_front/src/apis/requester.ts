c208f6deca1664a233f02384d753b279347296a1
woohm402 <woohm404@gmail.com>
2021-08-02 19:43:04 +0900

    문제 변경

--- waffle_recruit_front/src/apis/requester.ts
+++ waffle_recruit_front/src/apis/requester.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// TODO .env.production
+const isProduction = true;
+
 export const requester = axios.create({
-  baseURL: 'http://recruit-api.wafflestudio.com',
+  baseURL: isProduction ? 'http://recruit-api.wafflestudio.com' : 'http://localhost:8000',
   xsrfCookieName: 'csrftoken',
   xsrfHeaderName: 'X-CSRFToken',
   withCredentials: true,
