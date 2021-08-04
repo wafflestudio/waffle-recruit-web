9f91fff5ed86ea43f71ee752994a655291dc33c5
Hank Choi <zlzlqlzl1@naver.com>
2021-08-04 19:12:16 +0900

    Update admin page

--- waffle_recruit_server/check/admin.py
+++ waffle_recruit_server/check/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
 
 # Register your models here.
-from check.models import Solver, Profile
+from django.contrib.auth.models import User
 
-admin.site.register(Solver)
-admin.site.register(Profile)
+from check.models import Solver, Profile
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from django.contrib.admin import SimpleListFilter
+
+# below lines should be added
+
+class SolverInline(admin.TabularInline):
+    model = Solver
+
+
+class ProfileInline(admin.TabularInline):
+    model = Profile
+
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = [SolverInline,ProfileInline]
+    list_display = ("username","email","major","grade","solved","credential")
+
+    def major(self,obj):
+        return Profile.objects.get(user=obj).major
+
+    def grade(self,obj):
+        return Profile.objects.get(user=obj).grade
+
+    def credential(self,obj):
+        return Profile.objects.get(user=obj).credential
+
+    def solved(self, obj):
+        return list(map(lambda x:x['problem_num'],Solver.objects.filter(user=obj).values('problem_num').all()))
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
