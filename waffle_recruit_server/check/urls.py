6d73890b2f897182dc073f8b768bea9fdf8d1657
Sanggyu Lee <8325289+gyusang@users.noreply.github.com>
2021-08-05 01:07:08 +0900

    Revert "Add url back"
    This reverts commit 3135e6e4187ffe9b6d0520d3c5c649e9131580a8.

--- waffle_recruit_server/check/urls.py
+++ waffle_recruit_server/check/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('signin/', views.signin, name='signin'),
     path('signout/', views.signout, name='signout'),
     path('prob/<prob_num>/', views.problem, name='grade'),
-    path('prob/<prob_num>/result/', views.problem_task_status),
     path('solvers/<prob_num>/', views.prob_solvers, name='prob_solvers'),
     path('token/', views.token, name='token'),
     path('skel/<lang>', views.skeleton, name='skel')
