98529c0df00c1ddbe29817b1126fb1256675d58b
woohm402 <woohm404@gmail.com>
2021-08-05 20:56:19 +0900

    로컬 cors 대응

--- waffle_recruit_server/waffle_recruit_server/settings.py
+++ waffle_recruit_server/waffle_recruit_server/settings.py
@@ -120,6 +120,7 @@ CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
     'https://recruit.wafflestudio.com',
     'http://recruit.wafflestudio.com',
+    'http://localhost:3000',
 )
 CORS_ALLOW_CREDENTIALS = True
 
