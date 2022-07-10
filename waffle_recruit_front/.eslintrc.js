50e8b85a745fdc1927da77fc3b962ddbccd70dff
woohm402 <woohm404@gmail.com>
2022-07-10 22:35:56 +0900

    remove eslint explicit module boundary types rule

--- waffle_recruit_front/.eslintrc.js
+++ waffle_recruit_front/.eslintrc.js
@@ -39,6 +39,7 @@ module.exports = {
     '@typescript-eslint/no-empty-interface': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/interface-name-prefix': 'off',
     'import/order': [
       'error',
