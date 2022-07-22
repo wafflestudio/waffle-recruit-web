fe3d71c7a95ca69af17c069557fdc143ed6d9e45
designdefined.by@gmail.com <designdefined.by@gmail.com>
2022-07-22 22:21:51 +0900

    feat: prepare for jwt frontend

--- waffle_recruit_front/src/context/authContext.tsx
+++ waffle_recruit_front/src/context/authContext.tsx
@@ -7,34 +7,27 @@ interface AuthContext {
 
   jwt: string | null;
   setJwt: (e: string) => void;
-  csrf: string | null;
-  setCsrf: (e: string) => void;
 }
 
 const Context = createContext<AuthContext>({
   user: null,
-  csrf: null,
   jwt: null,
   setJwt: () => null,
   setUser: () => null,
   clearUser: () => null,
-  setCsrf: () => null,
 });
 
 export const AuthContextProvider = ({ children }: PropsWithChildren<unknown>) => {
   const [user, setUser] = useState<string | null>(null);
-  const [csrf, setCsrf] = useState<string | null>(null);
   const [jwt, setJwt] = useState<string | null>(null);
 
   return (
     <Context.Provider
       value={{
         user,
-        csrf,
         jwt,
         setJwt,
         setUser,
-        setCsrf,
         clearUser: () => setUser(null),
       }}
     >
--- waffle_recruit_front/src/pages/AuthorizedRouter.tsx
+++ waffle_recruit_front/src/pages/AuthorizedRouter.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useHistory } from 'react-router-dom';
 
 import ProblemPage from './main';
 import Submit from './submit';
 
 const AuthorizedRouter: React.FC = () => {
-  //check token logic
+  const history = useHistory();
+
+  //check login
+  useEffect(() => {
+    const isLoggedIn = true;
+
+    if (!isLoggedIn) {
+      history.push('/signin');
+    }
+  }, [history.location.pathname]);
 
   return (
     <>
       <Route path="/main" exact component={ProblemPage} />
-        <Route path="/coverletter" exact component={ProblemPage} />
+      <Route path="/coverletter" exact component={ProblemPage} />
       <Route path="/problem/:prob_num" exact component={ProblemPage} />
       <Route path="/problem/:prob_num/submit" exact component={Submit} />
       <Redirect to="/main" />
